838c0661c38f12e142493fca25f617474381a105
shaharth406 <shaharth406@yahoo.com>
2019-01-02 15:13:57 +0530

    procfs added

---
+++ Arth/procfs/.procfs.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/spanidea/Arth/procfs/procfs.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/spanidea/Arth/procfs/procfs.ko /home/spanidea/Arth/procfs/procfs.o /home/spanidea/Arth/procfs/procfs.mod.o
