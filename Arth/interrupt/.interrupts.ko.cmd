2d0998af71d1484402f587138e9e47d14a931030
shaharth406 <shaharth406@yahoo.com>
2019-01-02 15:13:10 +0530

    interrupt added

---
+++ Arth/interrupt/.interrupts.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/spanidea/Arth/interrupt/interrupts.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/spanidea/Arth/interrupt/interrupts.ko /home/spanidea/Arth/interrupt/interrupts.o /home/spanidea/Arth/interrupt/interrupts.mod.o
---
+++ Arth/interrupt/.tmp_versions/interrupts.mod
@@ -0,0 +1,2 @@
+/home/spanidea/Arth/interrupt/interrupts.ko
+/home/spanidea/Arth/interrupt/interrupts.o
